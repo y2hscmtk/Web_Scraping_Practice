6c34842c9d7495d14627d58a5ad21fabce5ee98d
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-01-30 16:25:09 +0900

    구글 리퀘스트 불러와서 파일생성

--- webscraping_basic/3_requests.py
+++ webscraping_basic/3_requests.py
@@ -1,7 +1,7 @@
 # 터미널에서 pip install requests
 import requests
 # 네이버로부터 request정보를 가져옴
-res = requests.get("https://naver.com")
+res = requests.get("https://www.google.com/")
 # html문서를 가져오는데 성공했다면 문제없이 진행되고, 그렇지 않을경우 에러를 일으킴
 res.raise_for_status()  # 4번과 6번줄의 코드는 쌍으로 함께쓴다고 생각할것
 # 6번 줄의 코드는 아래 주석과 같은 효과를 낸다.
@@ -13,5 +13,9 @@ res.raise_for_status()  # 4번과 6번줄의 코드는 쌍으로 함께쓴다고
 # else:
 #     print("스크래핑 불가능. [에러코드 : ", res.ststus_code, "]")
 
+print(len(res.text))  # 가져온 글자 수 확인
+print(res.text)
 
-print("웹 스크래핑을 진행합니다.")
+# 불러온 데이터를 파일로 만들어서 확인
+with open("mygoogle.html", "w", encoding="utf-8") as f:
+    f.write(res.text)  # 불러온 내용을 mygoogle.html파일에 작성
