d57ea936c057852b65784aa862c74c296eacf62b
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-03-05 19:44:59 +0900

    웹 스크래핑 연습 : 공지사항 날짜 정보 가져오기

--- webscraping_basic/bs4_practice_hansung.py
+++ webscraping_basic/bs4_practice_hansung.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 # 네이버웹툰의 홈페이지 구조가 변경됨에 따라 실습영상대로 하면 작동하지 않기때문에
 # 학교 홈페이지 정보를 가져오는것으로 실습을 대체함
 
-url = "http://cse.hansung.ac.kr/news?searchCondition"
+url = "http://cse.hansung.ac.kr/news?searchCondition=%EA%B3%B5%EC%A7%80%EC%82%AC%ED%95%AD"
 response = requests.get(url)
 response.raise_for_status()
 
@@ -13,7 +13,14 @@ soup = BeautifulSoup(response.text, "lxml")
 
 # 공지사항 목록 모두 가져오기
 # table에서 태그가 a인 모든 값 가져오기
-announcement = soup.table.find_all("a")
+announcement = soup.find_all("tr")
 
-for i in range(len(announcement)):
-    print(str(i+1) + announcement[i].get_text())
+for data in announcement:
+    if data.a == None:
+        continue
+    title = data.a.get_text()
+    date = data.find_all("td")[4].get_text()
+    link = "http://cse.hansung.ac.kr/" + data.a["href"]
+    print(date)
+    print(title)
+    print(link)
