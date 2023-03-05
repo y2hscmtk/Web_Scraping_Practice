9141caab2b73ef8c9212295d92eb3a62ab79c077
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-03-05 23:34:31 +0900

    비교과 포인트 목록 가져오기

--- webscraping_basic/bs4_practice_smart.py
+++ webscraping_basic/bs4_practice_smart.py
@@ -4,8 +4,25 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://hsportal.hansung.ac.kr/ko/program"
-res = requests.get(url)
-res.raise_for_status()  # 오류방지
+url_main = "https://hsportal.hansung.ac.kr/ko/program/all/list/all/"
 
-soup = BeautifulSoup(res.text, "lxml")
+# 5페이지의 정보를 계산
+for page in range(1, 7):
+    url = url_main + str(page)
+    res = requests.get(url)
+    res.raise_for_status()  # 오류방지
+
+    soup = BeautifulSoup(res.text, "lxml")
+
+    # 비교과 포인트, 마감일 정보
+    point_info = soup.find_all("label", attrs={"class": "APPROACH_CLOSING"})
+    # 제목 정보
+    title_info = soup.find_all("b", attrs={"class": "title"})
+
+    print(str(page) + "번째 페이지에 대한 정보입니다.")
+    for i in range(len(point_info)):
+        title = title_info[i].get_text()
+        Dday, point = point_info[i].get_text().split("p")
+        print(title)
+        print("마감일: " + Dday + " 포인트: " + point)
+    print("*************************************************")
